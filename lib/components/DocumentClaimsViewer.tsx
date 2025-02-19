df760896e2fd11d7aeeede7492871f3a06600dea
Minor Gordon <minorg@server.fake>
2025-02-18 20:43:33 -0500

    more work on claims viewer

--- lib/components/DocumentClaimsViewer.tsx
+++ lib/components/DocumentClaimsViewer.tsx
@@ -3,7 +3,6 @@
 import { AnnotateDocumentForm } from "@/lib/components/AnnotateDocumentForm";
 import { DocumentClaimsDataTable } from "@/lib/components/DocumentClaimsDataTable";
 import {
-  Claim,
   DocumentClaims,
   DocumentStub,
   UnevaluatedClaims,
@@ -46,27 +45,33 @@ export function DocumentClaimsViewer(json: {
   return (
     <div className="flex flex-col gap-4">
       {json.workflows.length > 0 ? (
-        claimsEvaluation !== null ? (
+        documentClaims.type === "EvaluatedClaims" ? (
           <div className="flex flex-row items-center justify-between">
             <div className="flex flex-row gap-2">
               <div>
                 <b>{translations("F1 score")}</b>:{" "}
-                {Math.round(claimsEvaluation.f1Score * 100.0) / 100.0}
+                {Math.round(
+                  ((2 * documentClaims.truePositiveClaims.length) /
+                    (2 * documentClaims.truePositiveClaims.length +
+                      documentClaims.falsePositiveClaims.length +
+                      documentClaims.falseNegativeClaims.length)) *
+                    100.0,
+                ) / 100.0}
               </div>
               <div>&middot;</div>
               <div>
                 <i>{translations("True positives")}</i>:{" "}
-                {Math.round(claimsEvaluation.truePositiveCount)}
+                {Math.round(documentClaims.truePositiveClaims.length)}
               </div>
               <div>&middot;</div>
               <div>
                 <i>{translations("False positives")}</i>:{" "}
-                {Math.round(claimsEvaluation.falsePositiveCount)}
+                {Math.round(documentClaims.falsePositiveClaims.length)}
               </div>
               <div>&middot;</div>
               <div>
                 <i>{translations("False negatives")}</i>:{" "}
-                {Math.round(claimsEvaluation.falseNegativeCount)}
+                {Math.round(documentClaims.falseNegativeClaims.length)}
               </div>
             </div>
             {annotateDocumentForm}
@@ -77,10 +82,7 @@ export function DocumentClaimsViewer(json: {
           </div>
         )
       ) : null}
-      <DocumentClaimsDataTable
-        claims={claims !== null ? claims : json.claims}
-        claimsEvaluation={claimsEvaluation}
-      />
+      <DocumentClaimsDataTable documentClaims={documentClaims.toJson()} />
     </div>
   );
 }
