37c45ed1d9fe19e55b75c46157c9409b7a014450
Minor Gordon <minorg@server.fake>
2025-03-01 18:14:15 -0500

    evaluateClaims test with Medline Plus claims

--- __tests__/unit/models/evaluateClaims.test.ts
+++ __tests__/unit/models/evaluateClaims.test.ts
@@ -1,3 +1,4 @@
+import { testData } from "@/__tests__/unit/data/testData";
 import {
   CategoricalValue,
   Claim,
@@ -119,5 +120,30 @@ describe("evaluateClaims", () => {
     expect(result.falseNegativeClaims).toHaveLength(1);
     expect(result.falsePositiveClaims).toHaveLength(0);
     expect(result.truePositiveClaims).toHaveLength(1);
+  });
+
+  it("MedlinePlus gold claims", ({ expect }) => {
+    const goldClaims = testData.medlinePlus.claims;
+    expect(goldClaims).toHaveLength(3);
+    const inferredClaims = [
+      new Claim({
+        subject: testData.medlinePlus.document.identifier,
+        predicate: dcterms.subject,
+        object: new CategoricalValue({
+          value: ConceptStub.create({
+            identifier: dataFactory.namedNode(
+              "https://medlineplus.gov/diabetesmellitus.html",
+            ),
+            prefLabel: [dataFactory.literal("Diabetes Mellitus")],
+          }),
+        }),
+      }),
+    ];
+    const evaluatedClaims = evaluateClaims(
+      goldClaims.concat(inferredClaims),
+    ).unsafeCoerce();
+    expect(evaluatedClaims.truePositiveClaims).toHaveLength(1);
+    expect(evaluatedClaims.falseNegativeClaims).toHaveLength(2);
+    expect(evaluatedClaims.falsePositiveClaims).toHaveLength(0);
   });
 });
